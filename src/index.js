791ba796558fe94e774039d0621d26199ab8a6c2
Eric Rogers <Rogers.eric2012@gmail.com>
2021-09-17 08:14:00 -0500

    Changed app to be a Contact Managing app

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-function Counter() {
-  const [counter , setCounter] = useState(0);
+function AddPersonForm(){
+  const [ person, setPerson ] = useState("");
 
-  useEffect(() => {
-    //alert("Number of clicks: " + counter)
-    //setCounter(counter+2)
-  }, [counter])
-
-  function increment() {
-    setCounter(counter+1);
+  function handleChange(e){
+    setPerson(e.target.value);
   }
 
-  return <div>
-    <p>{counter}</p>
-    <button onClick={increment}>Increment</button>
-  </div>
-  
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+      placeholder="Add new contact"
+      onChange={handleChange}
+      vlaue={person.name} />
+      <button type="submit">Add</button>
+    </form>
+  );
 }
 
-ReactDOM.render(<Counter />, document.getElementById('root'))
+function PeopleList(props){
+  const arr = props.data;
+  const listItems = arr.map((val, index) =>
+    <li key={index}>{val}</li>
+    );
+    return <ul>{listItems}</ul>;
+}
+
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+const el = (
+  <div>
+    <AddPersonForm />
+    <PeopleList data={contacts} />
+  </div>
+)
+
+ReactDOM.render(el, document.getElementById('root'))
