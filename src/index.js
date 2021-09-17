3fa04c3c33dd39ea53ae1e396f00c7fd9c902750
Eric Rogers <Rogers.eric2012@gmail.com>
2021-09-17 08:05:31 -0500

    add useEffect and commented out alert and infiniti loop

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 function Counter() {
   const [counter , setCounter] = useState(0);
+
+  useEffect(() => {
+    //alert("Number of clicks: " + counter)
+    //setCounter(counter+2)
+  }, [counter])
 
   function increment() {
     setCounter(counter+1);
   }
 
   return <div>
-    <p>
-      {counter}
-    </p>
-    <button onClick={increment}>
-      Increment
-    </button>
+    <p>{counter}</p>
+    <button onClick={increment}>Increment</button>
   </div>
+  
 }
 
 ReactDOM.render(<Counter />, document.getElementById('root'))
